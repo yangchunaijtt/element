251b822ec047aa22b0e321c4ade93b9dd039f553
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-04-30 15:27:12 +0800

    4.30

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,15 +13,25 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="description">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="supports-count" v-if="seller.supports" @click="showDetail">
+        <span class="number">{{seller.supports.length}}个</span>
+        <span class="icon-keyboard_arrow_right"></span>
+      </div>
     </div>
-    <div class='bulletin-wrapper'>
+    <div class='bulletin-wrapper' @click="showDetail">
       <span class="icon"></span>
-      <span class="bulletin">{{seller.bulletin}}</span>
-      <span class="svg icon-keyboard_arrow_right"></span>
+      <span class="bulletin" >{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img src="">
+    </div>
+    <div v-show="showDetails" class="details">
+
     </div>
   </div>
 </template>
@@ -33,7 +43,19 @@
         type:Object
       }
     },
-    
+    data(){
+      return {
+        showDetails:false
+      };
+    },
+    methods:{
+      showDetail(){
+        this.showDetails = true ;
+      },
+    },
+    created(){
+      this.classMap = ['decrease','discount','special','invoice','guarantee'];
+    }
   }
 </script>
 
@@ -44,11 +66,14 @@
     background:rgba(7,17,27,0.5)
     color:#fff
     .content-wrapper
+      position:relative
       padding:24px 12px 18px 24px 
       font-size:0px
       .avatar
         display:inline-block
-        vertical-align: top;
+        vertical-align: top
+        img 
+          border-radius:2px 
       .content
         display:inline-block
         font-size:14px
@@ -79,42 +104,78 @@
             width:12px 
             height:12px 
             vertical-align:top
-            bg-image("decrease_1")
             background-repeat:no-repeat
             background-size:12px 12px
+            &.decrease
+              bg-image("decrease_1")
+            &.discount
+              bg-image("discount_1")
+            &.guarantee
+              bg-image("guarantee_1")
+            &.invoice
+              bg-image("invoice_1")
+            &.special
+              bg-image("special_1")
           .description
             display:inline-block
             font-size:10px
             line-height: 12px;
             margin: 0px
             vertical-align: top;
+      .supports-count
+        position:absolute
+        height:24px 
+        padding:0px 8px 
+        line-height:24px 
+        bottom:18px 
+        right:12px 
+        font-size:10px 
+        background-color:rgba(0,0,0,0.2)
+        border-radius:14px 
+        .number 
+          vertical-align:top
+          margin-right:2px 
+          text-align:center
+        .icon-keyboard_arrow_right 
+          vertical-align:top
+          line-height:24px
     .bulletin-wrapper
+      position:relative
       height:28px 
       line-height:28px 
+      padding:0px 22px 0px 12px 
+      white-space: nowrap
+      overflow: hidden
+      text-overflow:ellipsis
       background-color:rgba(7,17,27,0.2)
-      padding:0px 12px 0px 12px 
-      font-size:0px
-      color:#fff
+      // font-size:0px
       .icon
         display:inline-block
         width:22px 
+        margin-top: 8px
+        vertical-align: top
         height:12px 
         bg-image("bulletin")
         background-repeat:no-repeat
         background-size:22px 12px
-        margin-top: 8px;
-        margin-right: 4px;
-        vertical-align: top;
       .bulletin
-        display:inline-block
-        overflow: hidden
-        text-overflow:ellipsis
-        white-space: nowrap
+        vertical-align: top
+        margin:0 4px 
         font-size:10px
-      .svg
-        display:inline-block
-        margin-left:8px
+      .icon-keyboard_arrow_right
+        position:absolute
+        right:12px 
+        top:8px
         font-size:10px
-        vertical-align: top;
-        line-height: 28px;
+    .background 
+      // 
+    .details
+      overflow:auto
+      position:fixed
+      top:0
+      left:0 
+      width:100% 
+      height:100% 
+      background:rgba(7,17,27,0.8)
+    
 </style>
--- src/https.js
+++ src/https.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import qs from 'qs'
 
-axios.defaults.timeout = 5000;                        //响应时间
+axios.defaults.timeout = 5000;                  //响应时间
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';        //配置请求头
 axios.defaults.baseURL = 'http://192.168.1.7:8090/';   //配置接口地址
 
